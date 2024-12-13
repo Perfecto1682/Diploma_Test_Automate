bb66d277ddf237de3d11d0c59a62e18f203d80ce
Perfecto1682 <perfecto_honrado5@mail.ru>
2024-12-13 13:00:04 +0300

    Update

--- aviasales_autotests/utils/webdriver_manager.py
+++ aviasales_autotests/utils/webdriver_manager.py
@@ -5,6 +5,5 @@ from webdriver_manager.chrome import ChromeDriverManager
 def get_driver():
     options = webdriver.ChromeOptions()
     options.add_argument("--start-maximized")
-    options.add_argument('--disable-gpu')  # Отключаем использование GPU
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     return driver
